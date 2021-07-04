d2ce6e75948792674818f965f279d2e189b90067
bruce <793041663@qq.com>
2021-07-04 17:15:20 +0800

    register button

--- pages/index.vue
+++ pages/index.vue
@@ -18,14 +18,23 @@
         </div>
       </div>
     </div>
+    <div class="updates">
+      <div class="container">
+        <h2>不想错过最新动态？立即注册获取免费账号！</h2>
+        <NuxtLink class="router-button" to="#">
+          注册Roller Grasses<Arrow class="arrow arrow-light" />
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import BlogPost from "@/components/BlogPost.vue"
 import BlogCard from "@/components/BlogCard.vue"
+import Arrow from "@/static/assets/icons/arrow-right-light.svg?inline"
 export default {
   name: "Home",
-  components: { BlogPost, BlogCard },
+  components: { BlogPost, BlogCard, Arrow },
   key(route) {
     return route.fullPath
   },
@@ -62,6 +71,42 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
